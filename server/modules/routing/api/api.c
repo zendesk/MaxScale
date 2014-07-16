84e3d3d34ce9fbc17bf804e35e47cb7b77ef79f7
counterpoint <counterpoint@aliro.org>
2014-07-16 14:11:50 +0100

    Basic PHP API integration

--- server/modules/routing/api/api.c
+++ server/modules/routing/api/api.c
@@ -31,6 +31,35 @@
 #include <log_manager.h>
 #include <sapi/embed/php_embed.h>
 
+#ifdef ZTS
+    void ***tsrm_ls;
+#endif
+
+    PHP_FUNCTION(sample_hello_world) {
+        php_printf("Hello World!\n");
+        ZVAL_LONG(return_value, 42);
+        return;
+    }
+    
+    static function_entry maxapi_functions[] = {
+        PHP_FE(sample_hello_world, NULL)
+        {   NULL, NULL, NULL    }
+    };
+    
+/* Extension bits */
+    zend_module_entry php_mymod_module_entry = {
+        STANDARD_MODULE_HEADER,
+        "maxapi",   /* extension name */
+        maxapi_functions,       /* function entries */
+        NULL,       /* MINIT */
+        NULL,       /* MSHUTDOWN */
+        NULL,       /* RINIT */
+        NULL,       /* RSHUTDOWN */
+        NULL,       /* MINFO */
+        "1.0",      /* Version */
+        STANDARD_MODULE_PROPERTIES
+    };    
+    
 static char *version_str = "V1.0.0";
 
 MODULE_INFO 	info = {
@@ -125,6 +154,11 @@ newSession(ROUTER *instance, SESSION *session)
 {
 static int i=0, *inst = &i;
 	/* Every time a user connects to the service */
+	int argc = 2;
+	char *argv[] = {"maxapi.php", "hello"};
+	
+        php_embed_init(argc, argv PTSRMLS_CC);
+        zend_startup_module(&php_mymod_module_entry);
 	session->state = SESSION_STATE_READY;
 	dcb_printf(session->client, "Welcome the SkySQL MaxScale API Interface (%s).\n",
 		version_str);
@@ -149,6 +183,7 @@ static void freeSession(
         void*   router_client_session)
 {
 	/* Called after all components of the session have been closed */
+        php_embed_shutdown(TSRMLS_C);
         return;
 }
 
@@ -158,8 +193,8 @@ routeQuery(ROUTER *instance, void *session, GWBUF *queue)
 	/* This happens for every request */
 	/* GWBUF passes data around - a buffer with start and end point - is a string */
 
-	int argc = 2;
 	char cmdbuf[80];
+	char *filename;
 	
 	/* Extract the characters */
 	while (queue)
@@ -168,21 +203,18 @@ routeQuery(ROUTER *instance, void *session, GWBUF *queue)
 		queue = gwbuf_consume(queue, GWBUF_LENGTH(queue));
 	}
 
-	char *argv[] = {"maxapi.php", cmdbuf};
-	
-	zend_file_handle script;
-	script.type = ZEND_HANDLE_FP;
-	script.filename = argv[0];
-	script.opened_path = NULL;
-	script.free_filename = 0;
-	if (!(script.handle.fp = fopen(script.filename, "rb"))) {
-		fprintf(stderr, "Unable to open: %s\n", argv[0]);
-		return -1;
-	}
-	
-	PHP_EMBED_START_BLOCK(argc, argv)
-		php_execute_script(&script TSRMLS_CC);
-	PHP_EMBED_END_BLOCK()
+	/* filename = strcat(strcat(getenv("MAXSCALE_HOME"), "/api/"), argv[0]); */
+	/* filename = strcat(strcat(getenv("MAXSCALE_HOME"), "/api/"), "maxapi.php"); */
+ 
+        zend_first_try {
+		char *include_script;
+		spprintf(&include_script, 0, "$argv[1] = '%s'; include '%s';", cmdbuf, "/home/mbrampton/MaxScaleHome/api/maxapi.php");
+		zend_eval_string(include_script, NULL, "/home/mbrampton/MaxScaleHome/api/maxapi.php" TSRMLS_CC);
+		efree(include_script);
+        } zend_catch {
+            int exit_status = EG(exit_status);
+            return exit_status;
+        } zend_end_try();
 	return 0;
 }
 

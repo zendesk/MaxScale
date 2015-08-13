38fdf7dcae1b7e75bf43bd88b76e1a54776428b6
Steven Davidovitz <steven.davidovitz@gmail.com>
2015-08-13 18:08:14 +0000

    indentation

--- server/modules/routing/flexmaster.c
+++ server/modules/routing/flexmaster.c
@@ -1,34 +1,3 @@
-/*
- * This file is distributed as part of MaxScale.  It is free
- * software: you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation,
- * version 2.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 51
- * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Copyright MariaDB Corporation Ab 2014
- */
-
-/**
- * @file cli.c - A "routing module" that in fact merely gives access
- * to a command line interface
- *
- * @verbatim
- * Revision History
- *
- * Date		Who		Description
- * 18/06/13	Mark Riddoch	Initial implementation
- * 13/06/14	Mark Riddoch	Creted from the debugcli
- *
- * @endverbatim
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -163,8 +132,8 @@ ROUTER_OBJECT *GetModuleObject() {
  * Create an instance of the router for a particular service
  * within the gateway.
  * 
- * @param service	The service this router is being create for
- * @param options	Any array of options for the query router
+ * @param service The service this router is being create for
+ * @param options Any array of options for the query router
  *
  * @return The instance data for this new instance
  */
@@ -182,8 +151,8 @@ static ROUTER *createInstance(SERVICE *service, char **options) {
 /**
  * Associate a new session with this instance of the router.
  *
- * @param instance	The router instance data
- * @param session	The session itself
+ * @param instance The router instance data
+ * @param session The session itself
  * @return Session specific data for this session
  */
 static void *newSession(ROUTER *instance, SESSION *session) {
@@ -192,16 +161,16 @@ static void *newSession(ROUTER *instance, SESSION *session) {
         if(flex_session == NULL)
                 return NULL;
 
-	flex_session->session = session;
-	return flex_session;
+        flex_session->session = session;
+        return flex_session;
 }
 
 /**
  * Close a session with the router, this is the mechanism
  * by which a router may cleanup data structure etc.
  *
- * @param instance		The router instance data
- * @param router_session	The session being closed
+ * @param instance  The router instance data
+ * @param router_session The session being closed
  */
 static void closeSession(ROUTER *instance, void *session) {
         free(session);
@@ -210,8 +179,8 @@ static void closeSession(ROUTER *instance, void *session) {
 /**
  * Free a debugcli session
  *
- * @param router_instance	The router session
- * @param router_client_session	The router session as returned from newSession
+ * @param router_instance The router session
+ * @param router_client_session The router session as returned from newSession
  */
 static void freeSession(ROUTER* router_instance, void *router_client_session) {
         return;
@@ -222,9 +191,9 @@ static void freeSession(ROUTER* router_instance, void *router_client_session) {
  * This is simply a case of sending it to the connection that was
  * chosen when we started the client session.
  *
- * @param instance		The router instance
- * @param router_session	The router session returned from the newSession call
- * @param queue			The queue of data buffers to route
+ * @param instance  The router instance
+ * @param router_session The router session returned from the newSession call
+ * @param queue   The queue of data buffers to route
  * @return The number of bytes sent
  */
 static int routeQuery(ROUTER *instance, void *session, GWBUF *queue) {
@@ -618,23 +587,23 @@ static MYSQL *mysql_connect(SERVICE *service, char *host, unsigned int port) {
 
         int timeout = 3;
 
-	if(mysql_options(connection, MYSQL_OPT_READ_TIMEOUT, (void *) &timeout) != 0) {
+        if(mysql_options(connection, MYSQL_OPT_READ_TIMEOUT, (void *) &timeout) != 0) {
                 error("Error: failed to set read timeout value for backend connection.");
                 mysql_close(connection);
                 return NULL;
-	}
-	
-	if(mysql_options(connection, MYSQL_OPT_CONNECT_TIMEOUT, (void *) &timeout) != 0) {
+        }
+
+        if(mysql_options(connection, MYSQL_OPT_CONNECT_TIMEOUT, (void *) &timeout) != 0) {
                 error("Error: failed to set connect timeout value for backend connection.");
                 mysql_close(connection);
                 return NULL;
-	}
-	
-	if(mysql_options(connection, MYSQL_OPT_WRITE_TIMEOUT, (void *) &timeout) != 0) {
+        }
+
+        if(mysql_options(connection, MYSQL_OPT_WRITE_TIMEOUT, (void *) &timeout) != 0) {
                 error("Error: failed to set connect timeout value for backend connection.");
                 mysql_close(connection);
                 return NULL;
-	}
+        }
 
         if(mysql_options(connection, MYSQL_OPT_USE_REMOTE_CONNECTION, NULL) != 0) {
                 error("Error: failed to set external connection. It is needed for backend server connections.");
@@ -691,8 +660,8 @@ static int parse_host_and_port(char *str, char **host, unsigned int *port) {
 /**
  * Display router diagnostics
  *
- * @param instance	Instance of the router
- * @param dcb		DCB to send diagnostics to
+ * @param instance Instance of the router
+ * @param dcb DCB to send diagnostics to
  */
 static void diagnostics(ROUTER *instance, DCB *dcb) {
         char date[64] = "";
